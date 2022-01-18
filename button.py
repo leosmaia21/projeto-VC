cae75df6b07c2967afcc12911b29bed9de4048cd
leosmaia21 <leosmaia21@gmail.com>
2022-01-18 14:18:37 +0000

    j

--- button.py
+++ button.py
@@ -18,4 +18,4 @@ class Button:
     def getValue(self,x,y,angle):
         if self.pos[0] <x<self.pos[0]+self.width and \
             self.pos[1] <y<self.pos[1]+self.height:
-                print('valor:',self.value,' angle:',angle)
+                print('value:',self.value,' angle:',angle)
